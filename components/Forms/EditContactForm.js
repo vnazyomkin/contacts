f6878414e74c090b0b708651452c7d3b8417eeb7
vnazemkin <v.nazyomkin@yandex.ru>
2021-01-04 17:28:28 +0300

    Добавил валидацию Yup

--- components/Forms/EditContactForm.js
+++ components/Forms/EditContactForm.js
@@ -1,22 +1,24 @@
 import {ErrorMessage, useFormik} from 'formik';
+import * as Yup from 'yup';
+import "yup-phone";
+
 
 export default function EditContactForm() {
-    const validate = value => {
-        const errors = {};
-        if (!value.name) {
-            errors.name = 'Поле "Имя" обязательно для заполнения';
-        } else if (value.name.length < 3) {
-            errors.name = 'Длина имени должна бытыь больше двух символов';
-        }
-        return errors;
-    };
+    const validationSchema = Yup.object({
+        name: Yup.string()
+            .min(3, 'Имя должно быть больше 2 символов')
+            .required('Имя обязательно для заполнения'),
+        phoneNum: Yup.string()
+            .phone(),
+
+    });
 
     const formik = useFormik({
       initialValues: {
         name: '',
         phoneNum: '',
       },
-      validate,
+      validationSchema,
       onSubmit: values => {
         alert(JSON.stringify(values, null, 3));
       },
@@ -29,18 +31,21 @@ export default function EditContactForm() {
         name="name" 
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.errors.handleBlur}
         value={formik.values.name} 
     />
-    {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+    {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+
     <label htmlFor="phoneNum">Телефон</label>
     <input
         id="phoneNum"
         name="phoneNum" 
         type="text"
         onChange={formik.handleChange}
+        onBlur={formik.errors.handleBlur}
         value={formik.values.phoneNum} 
     />
-    
+    {formik.errors.phoneNum ? <div>{formik.errors.phoneNum}</div> : null}
     <button type="submit">Сохранить</button>
     </form>
   );
